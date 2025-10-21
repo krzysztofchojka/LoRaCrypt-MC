bdc072e2b4f97ea21142e019fbb85ae8a6d2410d
krzysztofchojka <krzysio.chojka@gmail.com>
2025-10-21 13:44:52 +0200

    /send command for clients

--- server_commands.cpp
+++ server_commands.cpp
@@ -36,6 +36,28 @@ void handle_client_message(int ser, const unsigned char server_pk[PK_LEN],
         return;
     }
 
+    if (strncmp((const char*)msg, "/send ", 6) == 0) {
+                // format: /send <idx> <text>
+                int idx = -1;
+                int consumed = 0;
+                if (sscanf((const char*)msg+6, "%d%n", &idx, &consumed) >= 1) {
+                    const char *msg2 = (const char*)msg + 6 + consumed;
+                    while (*msg2 == ' ') msg2++;
+                    if (idx >= 0 && idx < client_count && *msg2) {
+                        if (server_send_to_client(ser, server_pk, idx, (const unsigned char*)msg2, strlen(msg2)) == 0) {
+                            printf("Sent to client %d\n", idx);
+                        } else {
+                            fprintf(stderr, "Failed to send to client %d\n", idx);
+                        }
+                    } else {
+                        printf("Invalid index or no message\n");
+                    }
+                } else {
+                    printf("Usage: /send <idx> <text>\n");
+                }
+                printf("loracrypt-server> "); fflush(stdout);
+            }
+
     if (strncmp((const char*)msg, "/long", 5) == 0) {
         char reply[1024];
         snprintf(reply, sizeof(reply), "andcuhsncisnciscmoskcmosvierdnvienvierunviuen 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199");
